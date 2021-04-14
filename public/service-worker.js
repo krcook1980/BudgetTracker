dac8b921f57fcb3f901c11d090b9c81a86cfdeba
Kelly Cook <kellyrcook1980@gmail.com>
2021-04-14 11:10:46 -0600

    manifest and server worker

---
+++ public/service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/index.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+//this installs the service worker
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListenter("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+//fetch
+self.addEventListener("fetch", function (evt) {
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => console.log(err))
+            })
+        );
+        return;
+    }
+
+    evt.respondWith(
+        caches.match(evt.request).then(function(response) {
+            return response || fetch(evt.request)
+        })
+    )
+
+})
